f9e5e7b55d519822f50b13a8b22cc7fbc46947de
erkandem <41091058+erkandem@users.noreply.github.com>
2019-02-25 18:39:47 +0100

    Rebuilt docs

--- d.m
+++
@@ -1,9 +0,0 @@
-root_path = pwd();
-
-rmpath(fullfile(root_path, 'swagccg'));
-rmpath(fullfile(root_path, 'swagccg', 'src'));
-rmpath(fullfile(root_path, 'swagccg', 'tests'));
-rmpath(fullfile(root_path, 'swagccg', 'rdir'));
-rmpath(fullfile(root_path, 'swagccg', 'jsonlab_master'));
-rmpath(fullfile(root_path, 'swagccg', 'urlread2'));
-rmpath(fullfile(root_path, 'swagccg', 'urlread2_fragments'));
